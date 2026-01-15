cd1907351c0c6accc0bd3cd8a700ebc84ecc4b47
André Luiz Santo <andreluizdev12@gmail.com>
2026-01-15 08:35:43 -0300

    hello repo

---
+++ api-getway/src/main/java/com/github/flimnk/api_getway/FallbackController.java
@@ -0,0 +1,16 @@
+package com.github.flimnk.api_getway;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+}
---
+++ api-getway/src/main/java/com/github/flimnk/api_getway/config/GetwayConfig.java
@@ -0,0 +1,27 @@
+package com.github.flimnk.api_getway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GetwayConfig {
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World")
+                                .addRequestHeader("LOOOSE","Vadia"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .host("*.circuitbreaker.com")
+                        .filters(f -> f.circuitBreaker(config -> config
+                                .setName("mycmd")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+}
